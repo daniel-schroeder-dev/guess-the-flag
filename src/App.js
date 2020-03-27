a70019605509a2bd2d78187b59f07f0ab3d7d862
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-27 15:40:54 -0500

    Implement logic to request flags and pull 4 random flags.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import RadioButtons from './components/RadioButtons/RadioButtons';
 
 import './App.css';
 
 function App() {
+
+  const [flags, setFlags] = useState([]);
+  const apiUrl = 'https://restcountries.eu/rest/v2/all?fields=name;flag;';
+
+
+  const getRandomNumbers = () => {
+
+    const randNums = [];
+    const NUM_RAND_NUMS = 4;
+    const TOTAL_NUM_FLAGS = 250;
+
+    for (let i = 0; i < NUM_RAND_NUMS; i++) {
+      let randNum = Math.floor(Math.random() * TOTAL_NUM_FLAGS);
+      if (randNums.includes(randNum)) {
+        --i;
+        continue;
+      }
+      randNums.push(randNum);
+    }
+
+    return randNums;
+
+  };
+
+  const getRandomFlags = flags => {
+    const randNums = getRandomNumbers();
+    return [flags[randNums[0]], flags[randNums[1]], flags[randNums[2]], flags[randNums[3]]];
+  };
+
+  useEffect(() => {
+
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(flags => {
+        setFlags(getRandomFlags(flags));
+      })
+      .catch(err => console.error(err));
+
+  }, []);
+
   return (
     <main className="app">
       <h1 className="app__title">Guess The Flag!</h1>
