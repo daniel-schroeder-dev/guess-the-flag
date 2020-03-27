0b264e9b7855822bcedd0feb5a30a4bb8d41ae86
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-27 14:51:22 -0500

    Add RadioButtons to App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import RadioButtons from './components/RadioButtons/RadioButtons';
+
 import './App.css';
 
 function App() {
   return (
     <main className="app">
       <h1 className="app__title">Guess The Flag!</h1>
+      <img src="https://restcountries.eu/data/afg.svg" alt="flag of Afganistan" />
+      <RadioButtons />
     </main>
   );
 }
