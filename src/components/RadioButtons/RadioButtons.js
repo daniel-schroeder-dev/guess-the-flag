91713d15a375cc4c6636f16bb2d588461d137cf7
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-27 15:52:38 -0500

    Dynamically generate radio buttons from flag data.

--- src/components/RadioButtons/RadioButtons.js
+++ src/components/RadioButtons/RadioButtons.js
@@ -6,43 +6,22 @@ import './RadioButtons.css';
 function RadioButtons(props) {
   return (
     <div className="radio-buttons">
-        <label htmlFor="rdo-1" className="btn-radio">
+
+      {props.flags.map(flag => (
+
+        <label key={flag.name} htmlFor="rdo-1" className="btn-radio">
           <input type="radio" id="rdo-1" name="radio-grp" />
           <svg width="20px" height="20px" viewBox="0 0 20 20">
             <circle cx="10" cy="10" r="9"></circle>
             <path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" className="inner"></path>
             <path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" className="outer"></path>
           </svg>
-          <span className="radio-buttons__option-name">Option One</span>
+          <span className="radio-buttons__option-name">{flag.name}</span>
         </label>
-        <label htmlFor="rdo-2" className="btn-radio">
-          <input type="radio" id="rdo-2" name="radio-grp" />
-          <svg width="20px" height="20px" viewBox="0 0 20 20">
-            <circle cx="10" cy="10" r="9"></circle>
-            <path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" className="inner"></path>
-            <path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" className="outer"></path>
-          </svg>
-          <span className="radio-buttons__option-name">Option Two</span>
-        </label>
-        <label htmlFor="rdo-3" className="btn-radio">
-          <input type="radio" id="rdo-3" name="radio-grp" />
-          <svg width="20px" height="20px" viewBox="0 0 20 20">
-            <circle cx="10" cy="10" r="9"></circle>
-            <path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" className="inner"></path>
-            <path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" className="outer"></path>
-          </svg>
-          <span className="radio-buttons__option-name">Option Three</span>
-        </label>
-        <label htmlFor="rdo-4" className="btn-radio">
-          <input type="radio" id="rdo-4" name="radio-grp" />
-          <svg width="20px" height="20px" viewBox="0 0 20 20">
-            <circle cx="10" cy="10" r="9"></circle>
-            <path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" className="inner"></path>
-            <path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" className="outer"></path>
-          </svg>
-          <span className="radio-buttons__option-name">Option Four</span>
-        </label>
-      </div>
+
+      ))}
+        
+    </div>
   );
 }
 
@@ -51,9 +30,3 @@ RadioButtons.propTypes = {
 }
 
 export default RadioButtons;
-
-
-
-
-
-
