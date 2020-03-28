10d2eff0f55a9ab9e7fe5696a37939d9fddbb5b3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-27 23:10:08 -0500

    Updates

---
+++ precache-manifest.bedfff43fcb4984b6c0a58ffddf9d5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2b31221d477689bf68747b0ed1bbde",
+    "url": "/guess-the-flag/index.html"
+  },
+  {
+    "revision": "cb9b36f1a087f4cfc157",
+    "url": "/guess-the-flag/static/css/main.5b4dca68.chunk.css"
+  },
+  {
+    "revision": "9cd8de8951343f569e75",
+    "url": "/guess-the-flag/static/js/2.a4a4e7b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-the-flag/static/js/2.a4a4e7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9b36f1a087f4cfc157",
+    "url": "/guess-the-flag/static/js/main.07fb007d.chunk.js"
+  },
+  {
+    "revision": "e35f1040b2500a40885b",
+    "url": "/guess-the-flag/static/js/runtime-main.2b7746c7.js"
+  }
+]);
