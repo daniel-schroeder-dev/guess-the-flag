cafd15c4539f6f597459184f62a8ee41d372361b
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-27 21:40:47 -0500

    Create PlayAgainButton component.

---
+++ src/components/PlayAgainButton/PlayAgainButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PlayAgainButton.css';
+
+function PlayAgainButton(props) {
+  return (
+    <button className="btn play-again-button" type="button">Play Again?</button>
+  );
+}
+
+export default PlayAgainButton;
